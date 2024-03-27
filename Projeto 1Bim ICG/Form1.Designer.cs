c394769c6e487c9e0160c3fecd9fb2108959ad5e
V! <125708505+Vitor-Lucas@users.noreply.github.com>
2024-03-27 10:29:15 -0300

    Add files via upload

---
+++ Projeto 1Bim ICG/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Projeto_1Bim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawButton = new System.Windows.Forms.Button();
+            this.b_box = new System.Windows.Forms.TextBox();
+            this.m_box = new System.Windows.Forms.TextBox();
+            this.FunctionLabel = new System.Windows.Forms.Label();
+            this.x_box = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.x2_box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DrawButton
+            // 
+            this.DrawButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.DrawButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.DrawButton.Location = new System.Drawing.Point(500, 401);
+            this.DrawButton.Name = "DrawButton";
+            this.DrawButton.Size = new System.Drawing.Size(126, 30);
+            this.DrawButton.TabIndex = 0;
+            this.DrawButton.Text = "Draw";
+            this.DrawButton.UseVisualStyleBackColor = true;
+            this.DrawButton.Click += new System.EventHandler(this.DrawButton_Click);
+            // 
+            // b_box
+            // 
+            this.b_box.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.b_box.Location = new System.Drawing.Point(278, 403);
+            this.b_box.Name = "b_box";
+            this.b_box.Size = new System.Drawing.Size(33, 26);
+            this.b_box.TabIndex = 1;
+            // 
+            // m_box
+            // 
+            this.m_box.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.m_box.Location = new System.Drawing.Point(206, 405);
+            this.m_box.Name = "m_box";
+            this.m_box.Size = new System.Drawing.Size(32, 26);
+            this.m_box.TabIndex = 2;
+            // 
+            // FunctionLabel
+            // 
+            this.FunctionLabel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.FunctionLabel.AutoSize = true;
+            this.FunctionLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.FunctionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FunctionLabel.Location = new System.Drawing.Point(327, 401);
+            this.FunctionLabel.Name = "FunctionLabel";
+            this.FunctionLabel.Size = new System.Drawing.Size(150, 28);
+            this.FunctionLabel.TabIndex = 3;
+            this.FunctionLabel.Text = "f(x) = mx + b";
+            // 
+            // x_box
+            // 
+            this.x_box.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.x_box.Location = new System.Drawing.Point(99, 353);
+            this.x_box.Name = "x_box";
+            this.x_box.Size = new System.Drawing.Size(32, 26);
+            this.x_box.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 356);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "X1:";
+            // 
+            // x2_box
+            // 
+            this.x2_box.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.x2_box.Location = new System.Drawing.Point(99, 401);
+            this.x2_box.Name = "x2_box";
+            this.x2_box.Size = new System.Drawing.Size(32, 26);
+            this.x2_box.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 404);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "X2:";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(174, 408);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "m:";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(250, 408);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "b:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.x2_box);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.x_box);
+            this.Controls.Add(this.FunctionLabel);
+            this.Controls.Add(this.m_box);
+            this.Controls.Add(this.b_box);
+            this.Controls.Add(this.DrawButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DrawButton;
+        private System.Windows.Forms.TextBox b_box;
+        private System.Windows.Forms.TextBox m_box;
+        private System.Windows.Forms.Label FunctionLabel;
+        private System.Windows.Forms.TextBox x_box;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox x2_box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Projeto 1Bim ICG/Form1.cs
@@ -0,0 +1,220 @@
+﻿/********************************************************************
+ *Colegio Técnico Antônio Teixeira Fernandes (Univap)
+ *Curso Técnico em Informática - Data de Entrega: 27 / 03 / 2024
+ * Autores do Projeto: Vitor Lucas Kohls Correa
+ *                     Isabelly Pacheco Marino
+ *
+ * Turma: 2F
+ * Atividade Proposta em aula
+ * Observação:
+ * 
+ * 
+ ********************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_1Bim
+{
+    public partial class Form1 : Form
+    {
+        int WIDTH;
+        int HEIGHT;
+
+        int m;
+        int b;
+
+        int retas_count = 1;
+
+        int offset = -25;
+
+        bool draw_line;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        Pen GetColor(int r, int g, int b)
+        {
+            Color color = Color.FromArgb(r, g, b);
+            return new Pen(color);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WIDTH = ActiveForm.Size.Width;
+            HEIGHT = ActiveForm.Size.Height;
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int[] MouseCoords = Form2PlaneCoords(MousePosition.X, MousePosition.Y);
+            Form1.ActiveForm.Text = "Plane Coords: {" + MouseCoords[0] + ", " + MouseCoords[1] + "} ";
+
+            MouseCoords = Plane2FormsCoords(MouseCoords[0], MouseCoords[1]);
+            Form1.ActiveForm.Text += "  Form Coords: {" + MouseCoords[0] + ", " + MouseCoords[1] + "} ";
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawAxis(e);
+
+            if (draw_line)
+            {
+                Pen pen = GetColor(0, 0, 0);
+                PrepareLine(pen, e);
+                draw_line = false;
+            }
+        }
+
+
+        int f(int x)
+        {
+            return m * x + b;
+        }
+        int[] Plane2FormsCoords(int x, int y)
+        {
+            y *= -1;
+            x += WIDTH / 2;
+            y += HEIGHT / 2;
+
+            return new int[] { x, y };
+        }
+        int[] Form2PlaneCoords(int x, int y)
+        { 
+            x -= WIDTH / 2;
+            y -= HEIGHT / 2;
+
+            return new int[] { x, y * -1 };
+        }
+
+
+        //Desenhos 
+        void DrawXMarks(PaintEventArgs e, Pen pen, int x)
+        {
+            int y1 = Plane2FormsCoords(0, 1)[1] + offset;
+            int y2 = Plane2FormsCoords(0, -1)[1] + offset;
+            DrawLine(e,pen, x, y1, x, y2);
+        }
+        void DrawYMarks(PaintEventArgs e, Pen pen, int y)
+        {
+            int x1 = Plane2FormsCoords(1, 0)[0];
+            int x2 = Plane2FormsCoords(-1, 0)[0];
+            DrawLine(e,pen, x1, y, x2, y);
+        }
+        void DrawAxis(PaintEventArgs e)
+        {
+            //Desenha as retas do exio X e do eixo Y
+            Pen pen = GetColor(0, 0, 0);
+
+            int xOrigin = WIDTH / 2;
+            int yOrigin = HEIGHT / 2;
+
+            DrawLine(e, pen, 0, yOrigin + offset, WIDTH, yOrigin + offset);// Eixo X
+            DrawLine(e, pen, xOrigin, 0, xOrigin, HEIGHT); // Eixo Y
+
+            for (int i = 0; i <= WIDTH; i += 10)
+                DrawXMarks(e,pen,i);
+
+            for(int i =0; i <= HEIGHT; i += 10)
+                DrawYMarks(e,pen,i);
+        }
+        void DrawLine(PaintEventArgs e, Pen pen,int x0,int y0, int x1, int y1)
+        {
+            e.Graphics.DrawLine(pen,x0,y0,x1,y1);
+        }
+        
+
+        void PrepareLine(Pen pen, PaintEventArgs e)
+        {
+            int x1 = int.Parse(x_box.Text);
+            int y1 = f(x1);
+            int x2 = int.Parse(x2_box.Text);
+            int y2 = f(x2);
+    
+            //Escreve no JSON os dados da reta
+            writeJSON(x1,y1,x2,y2);
+
+            int[] NewCoords = Plane2FormsCoords(x1, y1);
+            x1 = NewCoords[0];
+            y1 = NewCoords[1] +offset;
+
+            NewCoords = Plane2FormsCoords(x2, y2);
+            x2 = NewCoords[0];
+            y2 = NewCoords[1] + offset;
+
+            DrawLine(e,pen, x1, y1, x2, y2);
+        }
+        void writeJSON(int x1, int y1, int x2, int y2)
+        {
+            draw_line = false;
+            String[] lines = { };
+
+            if (retas_count != 1)
+            {
+                StreamReader file_read = new StreamReader("arquivo.json");
+
+                lines = new string[retas_count];
+
+                for (int i = 0; i < retas_count; i++)
+                    lines[i] = file_read.ReadLine();
+
+                file_read.Close();
+            }
+
+            StreamWriter file = new StreamWriter("file.json");
+
+            if (retas_count == 1)
+                file.WriteLine("{\"Pontos\":[");
+            
+            for (int i = 0; i < lines.Length; i++)
+                file.WriteLine(lines[i]);
+
+            String line = "\t{\"Line"+ retas_count+ "\":\"X1\",\"Y1\":“X2”,”Y2”:["+x1+","+y1+","+x2+","+y2+"]},";
+            file.WriteLine(line);
+
+            file.Write("]}");
+            file.Close();
+
+            retas_count++;
+        }
+        private void DrawButton_Click(object sender, EventArgs e)
+        {
+            if (m_box.Text == "")
+                m = 0;
+            else
+                m = int.Parse(m_box.Text);
+
+            if (b_box.Text == "")
+                b = 0;
+            else
+                b = int.Parse(b_box.Text);
+
+            if (x_box.Text == "" || x2_box.Text == "")
+            {
+                MessageBox.Show("Fill all fields");
+                return;
+            }
+
+            if(x_box.Text == x2_box.Text)
+            {
+                MessageBox.Show("X1 and X2 are equal, change one of it´s values");
+                return;
+            }
+                
+            draw_line = true;
+            Invalidate();
+
+            if (b < 0)
+                FunctionLabel.Text = "f(x) = " + m + "x -" + Math.Abs(b);
+            else
+                FunctionLabel.Text = "f(x) = " + m + "x +" + b;
+        }
+    }
+}
